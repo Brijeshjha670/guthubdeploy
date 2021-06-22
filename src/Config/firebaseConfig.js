840aa6244bc24e0a44b643efa8ac36bcaae6afc3
Brijeshjha670 <brijeshjha18011997@gmail.com>
2021-06-21 21:33:42 -0700

    completed

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyCZrhM105QMPxVgTd4wwH9SxJqN-yqE0RM',
+  authDomain: 'mygitapp-c8960.firebaseapp.com',
+  projectId: 'mygitapp-c8960',
+  storageBucket: 'mygitapp-c8960.appspot.com',
+  messagingSenderId: '71571744913',
+  appId: '1:71571744913:web:f50473b4eff00e46703730',
+  measurementId: 'G-80TS9L0QKN',
+};
